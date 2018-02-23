0ab0fdfed12bb7379a7f6bafc80cf500ac98f3c3
Unknown <me@ryanmcdonough.co.uk>
2018-02-23 11:12:04 +0000

    Finished.

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ *  Takes a string, returns super army soldier.
+ * @param {string} string
+ * @return {string}
+ */
+module.exports = function(string) {
+    return "Super Army Soldier";
+};
